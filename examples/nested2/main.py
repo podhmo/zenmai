91c16be15274c1cb5560572405b8777b5105bbd4
podhmo <ababjam61+github@gmail.com>
2017-04-20 01:11:55 +0900

    examples

--- examples/nested2/main.py
+++
@@ -1,9 +0,0 @@
-from zenmai.actions import load  # NOQA
-
-
-if __name__ == "__main__":
-    import zenmai
-    import sys
-    from dictknife import loading
-    d = zenmai.compilefile(sys.modules[__name__], sys.argv[1])
-    loading.dumpfile(d)
