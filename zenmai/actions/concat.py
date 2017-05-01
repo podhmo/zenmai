8e1ebee87c668eff7aed349b42a741f864fabfc5
podhmo <ababjam61+github@gmail.com>
2017-05-01 09:42:38 +0900

    adjustment

--- zenmai/actions/concat.py
+++ zenmai/actions/concat.py
@@ -1,11 +1,10 @@
 from dictknife import deepmerge
-from collections import OrderedDict
 
 
-def concat(ds):
+def concat(ds, override=False):
     """
     $concat:
       - name: foo
       - age: 10
     """
-    return deepmerge(OrderedDict(), *ds)
+    return deepmerge(*ds, override=override)
