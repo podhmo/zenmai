a237a49c01b02848ce7ed17259abfacb2fb923fd
podhmo <ababjam61+github@gmail.com>
2017-05-01 10:36:43 +0900

    load functions move to zenmai.loader

--- zenmai/__init__.py
+++ zenmai/__init__.py
@@ -1,14 +1,1 @@
-from io import StringIO
-from zenmai.driver import Driver
-from zenmai import actions
 from zenmai.compile import compile  # NOQA
-
-
-def load(rf, filename=None, data=None, driver_factory=Driver, module=actions):
-    filename = filename or getattr(rf, "name", None)
-    driver = driver_factory(module, filename, data=data)
-    return driver.transform(driver.load(rf))
-
-
-def loads(s, filename=None, data=None, driver_factory=Driver, module=actions):
-    return load(StringIO(), filename, data=data, driver_factory=driver_factory, module=module)
