f78a2b8d4f68dc6f16d7e4b94381bb22d2a94cf6
podhmo <ababjam61+github@gmail.com>
2017-05-03 17:53:29 +0900

    concat improvement

--- zenmai/actions/concat.py
+++ zenmai/actions/concat.py
@@ -2,10 +2,15 @@ from dictknife import deepmerge
 from zenmai.utils import quote
 
 
-def concat(ds, override=False):
+def concat(ds, override=False, sep="\n"):
     """
     $concat:
       - name: foo
       - age: 10
     """
-    return quote(deepmerge(*ds, override=override))
+    if not ds:
+        return {}
+    elif hasattr(ds[0], "keys"):
+        return quote(deepmerge(*ds, override=override))
+    else:
+        return sep.join(ds)
