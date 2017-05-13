1f5c72f49359c11ef3d09b3bb7c863c90091020f
podhmo <ababjam61+github@gmail.com>
2017-05-13 18:05:09 +0900

    faster entrypoints

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-
+import fastentrypoints
 
 from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
@@ -14,47 +14,44 @@ try:
 except IOError:
     README = CHANGES = ''
 
-
 install_requires = [
     "dictknife[load]",
-    "magicalimport>=0.3.0",
+    "magicalimport>=0.3.1",
     "jinja2",
     "colorama",
 ]
 
+docs_extras = []
 
-docs_extras = [
-]
+tests_require = []
 
-tests_require = [
-]
+testing_extras = tests_require + []
 
-testing_extras = tests_require + [
-]
-
-setup(name='zenmai',
-      version='0.3.0',
-      description='toy language on yaml or json',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-          "Programming Language :: Python",
-          "Programming Language :: Python :: Implementation :: CPython",
-      ],
-      keywords='yaml, json, toylang, minilang',
-      author="podhmo",
-      author_email="ababjam61+github@gmail.com",
-      url="https://github.com/podhmo/zenmai",
-      packages=find_packages(exclude=["zenmai.tests"]),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=install_requires,
-      extras_require={
-          'testing': testing_extras,
-          'docs': docs_extras,
-      },
-      tests_require=tests_require,
-      test_suite="zenmai.tests",
-      entry_points="""
+setup(
+    name='zenmai',
+    version='0.3.0',
+    description='toy language on yaml or json',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    keywords='yaml, json, toylang, minilang',
+    author="podhmo",
+    author_email="ababjam61+github@gmail.com",
+    url="https://github.com/podhmo/zenmai",
+    packages=find_packages(exclude=["zenmai.tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={
+        'testing': testing_extras,
+        'docs': docs_extras,
+    },
+    tests_require=tests_require,
+    test_suite="zenmai.tests",
+    entry_points="""
       [console_scripts]
       zenmai=zenmai.cmd:main
-""")
+"""
+)
